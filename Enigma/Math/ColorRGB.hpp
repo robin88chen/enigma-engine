69b412598eb6387df0adeb503e3872c4f5fb3b51
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2024-11-23 17:24:24 +0800

    [Math] add ColorRGB, ColorRGBA

---
+++ Enigma/Math/ColorRGB.hpp
@@ -0,0 +1,80 @@
+﻿/*********************************************************************
+ * \file   ColorRGB.hpp
+ * \brief
+ *
+ * \author Lancelot 'Robin' Chen
+ * \date   November 2024
+ *********************************************************************/
+#ifndef COLOR_RGB_HPP
+#define COLOR_RGB_HPP
+#include <array>
+namespace Math
+{
+    /** Color RGB Class
+    @remarks
+    The components are intended to be in the interval [0,1].
+    */
+    class ColorRGB
+    {
+    public:
+        ColorRGB(); ///< initial values (0,0,0)
+        ColorRGB(float r, float g, float b);
+        /// index -- 0: red, 1: green, 2: blue
+        ColorRGB(const std::array<float, 3>& c);
+        /// rgb = 0x00rrggbb
+        ColorRGB(unsigned rgb);
+
+        operator const float* () const;
+        operator float* ();
+        [[nodiscard]] float r() const;
+        void r(float r);
+        [[nodiscard]] float g() const;
+        void g(float g);
+        [[nodiscard]] float b() const;
+        void b(float b);
+        [[nodiscard]] unsigned rgb() const;
+
+        bool operator==(const ColorRGB& c) const;
+        bool operator!=(const ColorRGB& c) const;
+        bool operator<(const ColorRGB& c) const;
+        bool operator<=(const ColorRGB& c) const;
+        bool operator>(const ColorRGB& c) const;
+        bool operator>=(const ColorRGB& c) const;
+
+        ColorRGB operator+(const ColorRGB& c) const;
+        ColorRGB operator-(const ColorRGB& c) const;
+        ColorRGB operator*(const ColorRGB& c) const;
+        ColorRGB operator*(float scalar) const;
+
+        ColorRGB& operator+=(const ColorRGB& c);
+        ColorRGB& operator-=(const ColorRGB& c);
+        ColorRGB& operator*=(const ColorRGB& c);
+        ColorRGB& operator*=(float scalar);
+
+        // Transform the color channels to [0,1].  Clamp sets negative values to
+        // zero and values larger than one to one.  ScaleByMax assumes the color
+        // channels are nonnegative, finds the maximum color channel, and divides
+        // all channels by that value.
+        /** Transform the color channels to [0,1].
+        Clamp sets negative values to zero and values larger than one to one. */
+        [[nodiscard]] ColorRGB clamp() const;
+        /** Transform the color channels to [0,1].
+        ScaleByMax assumes the color channels are nonnegative, finds the maximum color channel, and divides
+         all channels by that value. */
+        [[nodiscard]] ColorRGB scaleByMax() const;
+
+        static const ColorRGB BLACK; ///< = (0,0,0) 
+        static const ColorRGB WHITE; ///< = (1,1,1)
+        static const ColorRGB INVALID; ///< = (-1,-1,-1)
+        static const ColorRGB ZERO; ///< = (0,0,0)
+
+    private:
+        std::array<float, 3> m_tuple;
+        unsigned m_rgb;
+    };
+
+    /** scalar * color */
+    ColorRGB operator*(float scalar, const ColorRGB& c);
+}
+
+#endif // COLOR_RGB_HPP
---
+++ Enigma/Math/ColorRGBA.hpp
@@ -0,0 +1,84 @@
+﻿/*********************************************************************
+ * \file   ColorRGBA.hpp
+ * \brief
+ *
+ * \author Lancelot 'Robin' Chen
+ * \date   November 2024
+ *********************************************************************/
+#ifndef COLOR_RGBA_HPP
+#define COLOR_RGBA_HPP
+#include <array>
+namespace Math
+{
+    /** Color RGB Class
+    @remarks
+    The components are intended to be in the interval [0,1].
+    */
+    class ColorRGBA
+    {
+    public:
+        // Construction.  The components are intended to be in the interval [0,1].
+        // For accessing of colors by array index, the map is 0 = red, 1 = green,
+        // 2 = blue and 3 = alpha
+        ColorRGBA(); ///< initial values (0,0,0,0)
+        ColorRGBA(float r, float g, float b, float a);
+        /// index -- 0: red, 1: green, 2: blue, 3: alpha
+        ColorRGBA(const std::array<float, 4>& c);
+        /// rgb = 0xaarrggbb
+        ColorRGBA(unsigned argb);
+
+        operator const float* () const;
+        operator float* ();
+        [[nodiscard]] float r() const;
+        void r(float r);
+        [[nodiscard]] float g() const;
+        void g(float g);
+        [[nodiscard]] float b() const;
+        void b(float b);
+        [[nodiscard]] float a() const;
+        void a(float a);
+        [[nodiscard]] unsigned argb() const;
+
+        bool operator==(const ColorRGBA& c) const;
+        bool operator!=(const ColorRGBA& c) const;
+        bool operator<(const ColorRGBA& c) const;
+        bool operator<=(const ColorRGBA& c) const;
+        bool operator>(const ColorRGBA& c) const;
+        bool operator>=(const ColorRGBA& c) const;
+
+        ColorRGBA operator+(const ColorRGBA& c) const;
+        ColorRGBA operator-(const ColorRGBA& c) const;
+        ColorRGBA operator*(const ColorRGBA& c) const;
+        ColorRGBA operator*(float scalar) const;
+
+        ColorRGBA& operator+=(const ColorRGBA& c);
+        ColorRGBA& operator-=(const ColorRGBA& c);
+        ColorRGBA& operator*=(const ColorRGBA& c);
+        ColorRGBA& operator*=(float scalar);
+
+        // Transform the color channels to [0,1].  Clamp sets negative values to
+        // zero and values larger than one to one.  ScaleByMax assumes the color
+        // channels are nonnegative, finds the maximum color channel, and divides
+        // all channels by that value.
+        /** Transform the color channels to [0,1].
+        Clamp sets negative values to zero and values larger than one to one. */
+        [[nodiscard]] ColorRGBA clamp() const;
+        /** Transform the color channels to [0,1].
+        ScaleByMax assumes the color channels are nonnegative, finds the maximum color channel, and divides
+        all channels by that value. */
+        [[nodiscard]] ColorRGBA scaleByMax() const;
+
+        static const ColorRGBA BLACK; ///< = (0,0,0,1)
+        static const ColorRGBA WHITE; ///< = (1,1,1,1)
+        static const ColorRGBA INVALID; ///< = (-1,-1,-1,-1)
+        static const ColorRGBA ZERO; ///< = (0,0,0,0)
+
+    private:
+        std::array<float, 4> m_tuple;
+        unsigned int m_argb;
+    };
+
+    ColorRGBA operator*(float scalar, const ColorRGBA& c);
+}
+
+#endif // COLOR_RGBA_HPP
