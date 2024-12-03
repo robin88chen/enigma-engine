6031870b61639091c5f32074d397f8092492c0c6
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2024-12-03 17:25:24 +0800

    [Collision] intersect box & sphere, sphere & sphere

---
+++ Enigma/Collision/IntrSphere2Sphere2.cpp
@@ -0,0 +1,25 @@
+﻿#include "IntrSphere2Sphere2.hpp"
+
+using namespace Collision;
+
+IntrSphere2Sphere2::IntrSphere2Sphere2(const Math::Sphere2& sphere0, const Math::Sphere2& sphere1) : m_sphere0(sphere0), m_sphere1(sphere1)
+{
+}
+
+const Math::Sphere2& IntrSphere2Sphere2::sphere0() const
+{
+    return m_sphere0;
+}
+
+const Math::Sphere2& IntrSphere2Sphere2::sphere1() const
+{
+    return m_sphere1;
+}
+
+bool IntrSphere2Sphere2::test()
+{
+    const Math::Vector2 center_diff = m_sphere1.center() - m_sphere0.center();
+    const float sqr_distance = center_diff.squaredLength();
+    const float sqr_radius_sum = (m_sphere0.radius() + m_sphere1.radius()) * (m_sphere0.radius() + m_sphere1.radius());
+    return sqr_distance <= sqr_radius_sum;
+}
---
+++ Enigma/Collision/IntrSphere2Sphere2.hpp
@@ -0,0 +1,31 @@
+﻿/*********************************************************************
+ * \file   IntrSphere2Sphere2.hpp
+ * \brief
+ *
+ * \author Lancelot 'Robin' Chen
+ * \date   December 2024
+ *********************************************************************/
+#ifndef INTR_SPHERE2_SPHERE2_HPP
+#define INTR_SPHERE2_SPHERE2_HPP
+#include "Intersector.hpp"
+#include "Math/Sphere2.hpp"
+
+namespace Collision
+{
+    class IntrSphere2Sphere2 : public Intersector
+    {
+    public:
+        IntrSphere2Sphere2(const Math::Sphere2& sphere0, const Math::Sphere2& sphere1);
+
+        [[nodiscard]] const Math::Sphere2& sphere0() const;
+        [[nodiscard]] const Math::Sphere2& sphere1() const;
+
+        bool test() override;
+
+    private:
+        Math::Sphere2 m_sphere0;
+        Math::Sphere2 m_sphere1;
+    };
+}
+
+#endif // INTR_SPHERE2_SPHERE2_HPP
