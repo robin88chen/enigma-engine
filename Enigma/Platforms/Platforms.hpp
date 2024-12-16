db04670810a5ba41643a00349c0e4660d0d3cdaf
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2024-12-16 17:30:17 +0800

    [Platforms] debug header and implements

---
+++ Enigma/Platforms/Platforms.hpp
@@ -0,0 +1,14 @@
+﻿/*****************************************************************
+ * \file   Platforms.hpp
+ * \brief  Platforms Module main header
+ *
+ * \author Lancelot 'Robin' Chen
+ * \date   December 2024
+ ******************************************************************/
+#ifndef PLATFORMS_MODULE_HPP
+#define PLATFORMS_MODULE_HPP
+
+#include "PlatformConfig.hpp"
+#include "Debug.hpp"
+
+#endif // PLATFORMS_MODULE_HPP
