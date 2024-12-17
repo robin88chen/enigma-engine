1fac3a6c8e87b7fa5c141ce63ae24202136493fd
Lancelot 'Robin' Chen <jongbin.chen@gmail.com>
2024-12-17 09:23:36 +0800

    [Platforms] logger, android bridge, java bridge

---
+++ Enigma/Platforms/Platforms.Android/AndroidBridge.cpp
@@ -0,0 +1,184 @@
+﻿#include "AndroidBridge.h"
+
+#if TARGET_PLATFORM == PLATFORM_ANDROID
+#include <android/log.h>
+#include <pthread.h>
+
+using namespace Platforms;
+
+static pthread_key_t g_vm_thread_key;
+
+void _detachCurrentThread(void* a)
+{
+    AndroidBridge::getJavaVM()->DetachCurrentThread();
+}
+
+AAssetManager* AndroidBridge::m_assetManager = nullptr;
+std::string AndroidBridge::m_dataFilePath;
+JavaVM* AndroidBridge::m_javaVM = nullptr;
+jobject AndroidBridge::m_activity = nullptr;
+std::unordered_map<JNIEnv*, std::vector<jobject>> AndroidBridge::m_localRefs;
+
+JavaVM* AndroidBridge::getJavaVM()
+{
+    pthread_t thisthread = pthread_self();
+    Debug::printf("GetJavaVM(), pthread_self() = %ld", thisthread);
+    return m_javaVM;
+}
+
+void AndroidBridge::setJavaVM(JavaVM* javaVM)
+{
+    pthread_t thisthread = pthread_self();
+    Debug::printf("SetJavaVM(%p), pthread_self() = %ld", javaVM, thisthread);
+    m_javaVM = javaVM;
+
+    pthread_key_create(&g_vm_thread_key, _detachCurrentThread);
+}
+
+JNIEnv* AndroidBridge::cacheEnv(JavaVM* jvm)
+{
+    JNIEnv* _env = nullptr;
+    // get jni environment
+    jint ret = jvm->GetEnv((void**)&_env, JNI_VERSION_1_4);
+
+    switch (ret)
+    {
+    case JNI_OK:
+        // Success!
+        pthread_setspecific(g_vm_thread_key, _env);
+        return _env;
+
+    case JNI_EDETACHED:
+        // Thread not attached
+        if (jvm->AttachCurrentThread(&_env, nullptr) < 0)
+        {
+            Debug::errorPrintf("Failed to get the environment using AttachCurrentThread()");
+            return nullptr;
+        }
+        else
+        {
+            // Success : Attached and obtained JNIEnv!
+            pthread_setspecific(g_vm_thread_key, _env);
+            return _env;
+        }
+
+    case JNI_EVERSION:
+        // Cannot recover from this error
+        Debug::errorPrintf("JNI interface version 1.4 not supported");
+    default:
+        Debug::errorPrintf("Failed to get the environment using GetEnv()");
+        return nullptr;
+    }
+}
+
+JNIEnv* AndroidBridge::getEnv()
+{
+    JNIEnv* _env = (JNIEnv*)pthread_getspecific(g_vm_thread_key);
+    if (_env == nullptr)
+        _env = AndroidBridge::cacheEnv(m_javaVM);
+    return _env;
+}
+
+jobject AndroidBridge::getActivity()
+{
+    return m_activity;
+}
+
+AAssetManager* AndroidBridge::getAAssetManager()
+{
+    return m_assetManager;
+}
+
+void AndroidBridge::setAAssetManager(AAssetManager* aasset_mgr)
+{
+    m_assetManager = aasset_mgr;
+}
+
+const std::string& AndroidBridge::getDataFilePath()
+{
+    return m_dataFilePath;
+}
+
+void AndroidBridge::setDataFilePath(const std::string& file_path)
+{
+    m_dataFilePath = file_path;
+}
+
+std::string AndroidBridge::stringFromJString(JNIEnv* env, jstring str)
+{
+    jboolean isCopy;
+    const char* native_str = env->GetStringUTFChars(str, &isCopy);
+    if (native_str == 0)
+    {
+        return "String from jstring null";
+    }
+    std::string ret_str(native_str);
+    env->ReleaseStringUTFChars(str, native_str);
+    return ret_str;
+}
+
+bool AndroidBridge::getStaticMethodInfo(JniMethodInfo& methodinfo,
+    const char* className, const char* methodName, const char* paramCode)
+{
+    if ((nullptr == className) || (nullptr == methodName) ||
+        (nullptr == paramCode))
+    {
+        return false;
+    }
+
+    JNIEnv* env = getEnv();
+    if (!env)
+    {
+        Debug::errorPrintf("Failed to get JNIEnv");
+        return false;
+    }
+
+    jclass classID = env->FindClass(className);
+    if (!classID) {
+        Debug::errorPrintf("Failed to find class %s", className);
+        env->ExceptionClear();
+        return false;
+    }
+
+    jmethodID methodID = env->GetStaticMethodID(classID, methodName, paramCode);
+    if (!methodID) {
+        Debug::errorPrintf("Failed to find static method id of %s", methodName);
+        env->ExceptionClear();
+        return false;
+    }
+
+    methodinfo.m_classID = classID;
+    methodinfo.m_env = env;
+    methodinfo.m_methodID = methodID;
+    return true;
+}
+
+jstring AndroidBridge::convert(JniMethodInfo& t, const char* x)
+{
+    jstring ret = t.m_env->NewStringUTF(x ? x : "");
+    m_localRefs[t.m_env].push_back(ret);
+    return ret;
+}
+
+jstring AndroidBridge::convert(JniMethodInfo& t, const std::string& x)
+{
+    return convert(t, x.c_str());
+}
+
+void AndroidBridge::deleteLocalRefs(JNIEnv* env)
+{
+    if (!env)
+    {
+        return;
+    }
+
+    for (const auto& ref : m_localRefs[env])
+    {
+        env->DeleteLocalRef(ref);
+    }
+    m_localRefs[env].clear();
+}
+
+#include "AndroidBridgeJni.inl"
+
+#endif
---
+++ Enigma/Platforms/Platforms.Android/AndroidBridge.h
@@ -0,0 +1,123 @@
+﻿#ifndef _ANDROID_BRIDGE_H
+#define _ANDROID_BRIDGE_H
+
+#include "../PlatformConfig.hpp"
+#include "../Debug.hpp"
+
+#if TARGET_PLATFORM == PLATFORM_ANDROID
+
+#include <android/asset_manager.h>
+#include <string>
+#include <jni.h>
+#include <unordered_map>
+#include <vector>
+
+namespace Platforms
+{
+    class AndroidBridge
+    {
+    public:
+        struct JniMethodInfo
+        {
+            JNIEnv* m_env;
+            jclass      m_classID;
+            jmethodID   m_methodID;
+        };
+    public:
+        static void setJavaVM(JavaVM* javaVM);
+        static JavaVM* getJavaVM();
+        static JNIEnv* getEnv();
+        static jobject getActivity();
+
+        static AAssetManager* getAAssetManager();
+        static void setAAssetManager(AAssetManager* aasset_mgr);
+
+        static const std::string& getDataFilePath();
+        static void setDataFilePath(const std::string& file_path);
+
+        static std::string stringFromJString(JNIEnv* env, jstring str);
+
+        static bool getStaticMethodInfo(JniMethodInfo& methodinfo, const char* className,
+            const char* methodName, const char* paramCode);
+
+        template <typename... Ts>
+        static void callStaticVoidMethod(const std::string& className,
+            const std::string& methodName, Ts... xs)
+        {
+            JniMethodInfo t;
+            std::string signature = "(" + std::string(getJNISignature(xs...)) + ")V";
+            if (getStaticMethodInfo(t, className.c_str(), methodName.c_str(), signature.c_str()))
+            {
+                t.m_env->CallStaticVoidMethod(t.m_classID, t.m_methodID, Convert(t, xs)...);
+                t.m_env->DeleteLocalRef(t.m_classID);
+                deleteLocalRefs(t.m_env);
+            }
+            else
+            {
+                Debug::errorPrintf("Failed to find static java method. Class name: %s, method name: %s, signature: %s ",
+                    className.c_str(), methodName.c_str(), signature.c_str());
+            }
+        }
+        template <typename... Ts>
+        static int callStaticIntMethod(const std::string& className,
+            const std::string& methodName, Ts... xs)
+        {
+            jint ret = 0;
+            JniMethodInfo t;
+            std::string signature = "(" + std::string(getJNISignature(xs...)) + ")I";
+            if (getStaticMethodInfo(t, className.c_str(), methodName.c_str(), signature.c_str()))
+            {
+                ret = t.m_env->CallStaticIntMethod(t.m_classID, t.m_methodID, Convert(t, xs)...);
+                t.m_env->DeleteLocalRef(t.m_classID);
+                deleteLocalRefs(t.m_env);
+            }
+            else
+            {
+                Debug::errorPrintf("Failed to find static java method. Class name: %s, method name: %s, signature: %s ",
+                    className.c_str(), methodName.c_str(), signature.c_str());
+            }
+            return ret;
+        }
+    private:
+        static JNIEnv* cacheEnv(JavaVM* jvm);
+        static std::string getJNISignature() { return ""; }
+        static std::string getJNISignature(bool) { return "Z"; }
+        static std::string getJNISignature(char) { return "C"; }
+        static std::string getJNISignature(short) { return "S"; }
+        static std::string getJNISignature(int) { return "I"; }
+        static std::string getJNISignature(long) { return "J"; }
+        static std::string getJNISignature(float) { return "F"; }
+        static  std::string getJNISignature(double) { return "D"; }
+        static std::string getJNISignature(const char*) { return "Ljava/lang/String;"; }
+        static std::string getJNISignature(const std::string&) { return "Ljava/lang/String;"; }
+        template <typename T>
+        static std::string getJNISignature(T x)
+        {
+            // This template should never be instantiated
+            static_assert(sizeof(x) == 0, "Unsupported argument type");
+            return "";
+        }
+        template <typename T, typename... Ts>
+        static std::string getJNISignature(T x, Ts... xs)
+        {
+            return getJNISignature(x) + getJNISignature(xs...);
+        }
+        static jstring convert(JniMethodInfo& t, const char* x);
+        static jstring convert(JniMethodInfo& t, const std::string& x);
+        template <typename T>
+        static T convert(JniMethodInfo&, T x) {
+            return x;
+        }
+        static void deleteLocalRefs(JNIEnv* env);
+
+    private:
+        static AAssetManager* m_assetManager;
+        static std::string m_dataFilePath;
+        static JavaVM* m_javaVM;
+        static jobject m_activity;
+        static std::unordered_map<JNIEnv*, std::vector<jobject>> m_localRefs;
+    };
+};
+#endif
+
+#endif // !_ANDROID_BEIDGE_H
